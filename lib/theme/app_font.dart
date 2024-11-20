7bd0122129effc8d257fc48362ccc21b63ad8627
LucasPetruci <lucaspetruci100@gmail.com>
2024-11-20 18:08:56 -0300

    feat(theme): refactor text styles to accept color parameter

--- lib/theme/app_font.dart
+++ lib/theme/app_font.dart
@@ -1,26 +1,35 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class AppFont {
   static const String helvetica = 'Helvetica';
 
-  //h1
-  static TextStyle h1 = TextStyle(
-    fontFamily: helvetica,
-    fontSize: 20,
-    fontWeight: FontWeight.w500,
-  );
+  // h1
+  static TextStyle h1({Color color = Colors.black}) {
+    return TextStyle(
+      fontFamily: helvetica,
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
 
-  //h2
-  static TextStyle h2 = TextStyle(
-    fontFamily: helvetica,
-    fontSize: 16,
-    fontWeight: FontWeight.w500,
-  );
+  // h2
+  static TextStyle h2({Color color = Colors.black}) {
+    return TextStyle(
+      fontFamily: helvetica,
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: color,
+    );
+  }
 
-  //h3
-  static TextStyle h3 = TextStyle(
-    fontFamily: helvetica,
-    fontSize: 14,
-    fontWeight: FontWeight.w400,
-  );
+  // h3
+  static TextStyle h3({Color color = Colors.black}) {
+    return TextStyle(
+      fontFamily: helvetica,
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: color,
+    );
+  }
 }
