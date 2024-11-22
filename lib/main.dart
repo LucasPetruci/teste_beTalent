0b2481a429403341d5b32caae03dbc681590aff1
LucasPetruci <lucaspetruci100@gmail.com>
2024-11-21 21:09:07 -0300

    feat: add splash screen support to android 12 and set condition to splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:be_talent/pages/employees_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+  initialization();
+}
+
+Future<void> initialization() async {
+  await Future.delayed(const Duration(seconds: 3));
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
