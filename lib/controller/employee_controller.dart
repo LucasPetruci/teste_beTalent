2832f0f9c264189f2fcc49c0da5d720e7efe912c
LucasPetruci <lucaspetruci100@gmail.com>
2024-11-25 17:12:21 -0300

    feat: add loading indicator to employee fetching process

--- lib/controller/employee_controller.dart
+++ lib/controller/employee_controller.dart
@@ -1,16 +1,24 @@
+import 'package:flutter/foundation.dart';
+
 import '../model/employee.dart';
 import '../service/employee_service.dart';
 
 class EmployeeController {
   final EmployeeService _employeeService;
+  final ValueNotifier<bool> isLoadingNotifier = ValueNotifier(false);
 
   EmployeeController(this._employeeService);
 
+  //get employees
   Future<List<Employee>> fetchEmployees() async {
+    isLoadingNotifier.value = true;
+
     try {
       return await _employeeService.fetchEmployees();
     } catch (e) {
       throw Exception('Error in EmployeeController: $e');
+    } finally {
+      isLoadingNotifier.value = false;
     }
   }
 }
--- lib/pages/employees_page.dart
+++ lib/pages/employees_page.dart
@@ -47,6 +47,7 @@ class _EmployeesPageState extends State<EmployeesPage> {
 
   Future<void> getEmployees() async {
     try {
+      _employeeController.isLoadingNotifier.value = true;
       final fetchedEmployees = await _employeeController.fetchEmployees();
       setState(() {
         employees = fetchedEmployees;
@@ -54,6 +55,8 @@ class _EmployeesPageState extends State<EmployeesPage> {
       });
     } catch (e) {
       print('Erro ao buscar funcionários: $e');
+    } finally {
+      _employeeController.isLoadingNotifier.value = false;
     }
   }
 
@@ -78,93 +81,105 @@ class _EmployeesPageState extends State<EmployeesPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Funcionários',
-                style: AppFont.h1(color: AppColors.black),
-                textAlign: TextAlign.start,
-              ),
-              const SizedBox(height: 12),
-              // Barra de pesquisa
-              TextField(
-                controller: searchController,
-                decoration: InputDecoration(
-                  hintText: 'Pesquisar',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(32),
-                    borderSide: BorderSide(
-                      color: AppColors.gray10,
+        child: ValueListenableBuilder<bool>(
+          valueListenable: _employeeController.isLoadingNotifier,
+          builder: (context, isLoading, child) {
+            if (isLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Funcionários',
+                    style: AppFont.h1(color: AppColors.black),
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(height: 12),
+                  // Barra de pesquisa
+                  TextField(
+                    controller: searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Pesquisar',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                        borderSide: BorderSide(
+                          color: AppColors.gray10,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 14),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: AppColors.gray10, width: 1.0),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: filteredEmployees.isEmpty
-                    ? Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          'Nenhum funcionário encontrado',
-                          style: AppFont.h2(color: AppColors.gray10),
-                        ),
-                      )
-                    : Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          // Cabeçalho da lista
-                          Container(
-                            height: 47,
-                            decoration: BoxDecoration(
-                              color: AppColors.gray10,
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(8),
-                                topRight: Radius.circular(8),
-                              ),
+                  const SizedBox(height: 14),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: AppColors.gray10, width: 1.0),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: filteredEmployees.isEmpty
+                        ? Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              'Nenhum funcionário encontrado',
+                              style: AppFont.h2(color: AppColors.gray10),
                             ),
-                            child: Row(
-                              children: [
-                                const SizedBox(width: 24),
-                                Text("Foto",
-                                    style: AppFont.h2(color: AppColors.black)),
-                                const SizedBox(width: 34),
-                                Text("Nome",
-                                    style: AppFont.h2(color: AppColors.black)),
-                                const Spacer(),
-                                Container(
-                                  width: 8,
-                                  height: 8,
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: AppColors.black,
+                          )
+                        : Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              // Cabeçalho da lista
+                              Container(
+                                height: 47,
+                                decoration: BoxDecoration(
+                                  color: AppColors.gray10,
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(8),
+                                    topRight: Radius.circular(8),
                                   ),
                                 ),
-                                const SizedBox(width: 36),
-                              ],
-                            ),
+                                child: Row(
+                                  children: [
+                                    const SizedBox(width: 24),
+                                    Text("Foto",
+                                        style:
+                                            AppFont.h2(color: AppColors.black)),
+                                    const SizedBox(width: 34),
+                                    Text("Nome",
+                                        style:
+                                            AppFont.h2(color: AppColors.black)),
+                                    const Spacer(),
+                                    Container(
+                                      width: 8,
+                                      height: 8,
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: AppColors.black,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 36),
+                                  ],
+                                ),
+                              ),
+                              // Lista de funcionários
+                              ListView.builder(
+                                itemCount: filteredEmployees.length,
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemBuilder: (context, index) {
+                                  return EmployeeCard(
+                                      employee: filteredEmployees[index]);
+                                },
+                              ),
+                            ],
                           ),
-                          // Lista de funcionários
-                          ListView.builder(
-                            itemCount: filteredEmployees.length,
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemBuilder: (context, index) {
-                              return EmployeeCard(
-                                  employee: filteredEmployees[index]);
-                            },
-                          ),
-                        ],
-                      ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
